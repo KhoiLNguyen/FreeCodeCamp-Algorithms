ffa0804fc764c35cc0bba8a3f62726b68c0715e0
Khoi <khoin0412@gmail.com>
2019-07-26 13:21:30 -0500

    Updated another solution

--- steamRoller.js
+++ steamRoller.js
@@ -1,7 +1,27 @@
+
 function steamrollArray(arr) {
   // I'm a steamroller, baby
+  let flattenArray = [];
+  
+  flattenArray(arr);
+
+  function flatten(arr) {
+    // goes through each value of array check if it is array or not
+    // if not push to flattenArray
+    arr.forEach((arr2) => {
+      if(!Array.isArray[arr2]) {
+        flattenArray.push(arr2);
+      } else { // if it is pass that array to flatten function again to flatten it
+        flatten(arr2);
+      }
+    })
+  }
+
+  return flattenArray;
+
+  // Another way to do it
   // .flat() flatten array of arrays based on the depth you specify
-  return arr.flat(5);
+  // return arr.flat(5);
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
